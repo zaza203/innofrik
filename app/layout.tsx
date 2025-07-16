c7f389110ea96b37de2d46293115e228a7fe4b93
Ihimbru Zadolf <ihimbruzadolf@gmail.com>
2025-07-16 10:15:04 +0100

    added facebook pixel in the layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,8 +84,27 @@ export default function RootLayout({
             ]
           })}
         </Script>
+
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+              n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1260309229084305');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
+        <noscript><img height="1" width="1" style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1260309229084305&ev=PageView&noscript=1"
+        /></noscript>
+
         <Navbar />
         <main className="min-h-screen">
           {children}
